c9c6a917b02d7da56ee30b1300ecc3b7532ff0ad
Robbie Jaeger <jaeger.rob@gmail.com>
2020-09-14 16:29:39 -0600

    Add gitignore, graphql and express libraries, and boilerplate server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+
+  }
+`);
+
+const root = {
+
+};
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
+});
